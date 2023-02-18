1384826c7ef5599f62f21f831a0ba28c89f79d81
Fares Alharbi <engfares@Faress-MacBook-Pro.local>
2023-02-18 21:27:45 +0300

    now we added all the functionality to add text field and information

--- api/portfolio/views.py
+++ api/portfolio/views.py
@@ -1,13 +1,39 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from .serializers import UserSerializer
 
-
-class UserViewSet(viewsets.ModelViewSet):
+from .models import Education, Work, Portfolio
+from .serializers import EducationSerializer, WorkSerializer, PortfolioSerializer
+class UserViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [AllowAny]
+
+
+class EducationViewSet(ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class WorkViewSet(ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class PortfolioViewSet(ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
