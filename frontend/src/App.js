9d48af7956874637893dc2ecfd778f0924587c25
Fares Alharbi <engfares@Faress-MacBook-Pro.local>
2023-02-19 09:46:13 +0300

    Build the Frontend with Tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,104 @@
-
+import Nav from './components/nav'
 import './App.css';
 
 function App() {
   return (
-    <div class="text-center text-5xl">
-      Home
-    </div>
+    <>
+      <Nav />
+
+      <div className='mb-40'>
+        <div className='w-2/4 mx-auto mt-10 mb-3'>
+          <h1 className='text-5xl mb-3'>Fares Alharbi</h1>
+          <p className='text-2xl text-cyan-900 ml-2'>Software Engineer</p>
+        </div>
+
+        <div className='flex space-between w-2/4 m-auto py-5 border rounded-sm ppx-3 shadow'>
+          <img alt='profile' className='rounded-full w-64 h-64 mx-8' src='./logo.png' />
+          <div>
+            <h5 className='text-2xl text-cyan-900 border-b-2 border-slate-300'>Biography</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='mb-40'>
+        <div className='mb-5 mx-5'>
+          <h1 className='text-5xl mb-3'>My Education</h1>
+          <p className='text-lg text-cyan-900'>Here is the education that I have received</p>
+        </div>
+        
+
+        <div className='grid grid-cols-3 gap-4 mx5'>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
+            <h5 className='py-2'>2017 - 2020</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
+            <h5 className='py-2'>2017 - 2020</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
+            <h5 className='py-2'>2017 - 2020</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='mb-40'>
+        <div className='mb-5 mx-5'>
+          <h1 className='text-5xl mb-3'>My Work Experience</h1>
+          <p className='text-lg text-cyan-900'>Here is some of my most recent relevant work experience</p>
+        </div>
+        
+
+        <div className='grid grid-cols-3 gap-4 mx5'>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Software Engineer</h3>
+            <h5 className='py-2'>2017 - 2020</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Software Engineer</h3>
+            <h5 className='py-2'>2017 - 2020</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Software Engineer</h3>
+            <h5 className='py-2'>2017 - 2020</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='mb-40'>
+        <div className='mb-5 mx-5'>
+          <h1 className='text-5xl mb-3'>My Portfolio</h1>
+          <p className='text-lg text-cyan-900'>Here is some of my most recent projects that I have built and the  link code in Github</p>
+        </div>
+        
+
+        <div className='grid grid-cols-3 gap-4 mx5'>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Tilte</h3>
+            <h5 className='py-2 text-blue-500 hover:text-blue-700 transition cursor-pointer'>View Code</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Tilte</h3>
+            <h5 className='py-2 text-blue-500 hover:text-blue-700 transition cursor-pointer'>View Code</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+          <div className='border rounded-sm p-3 shadow'>
+            <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Tilte</h3>
+            <h5 className='py-2 text-blue-500 hover:text-blue-700 transition cursor-pointer'>View Code</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
