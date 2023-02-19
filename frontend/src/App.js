45fec487ce12a839db34fc29540a6a8816e3dd8a
Fares Alharbi <engfares@Faress-MacBook-Pro.local>
2023-02-19 11:33:07 +0300

    Mobile Responsiveness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import Nav from './components/nav'
 import './App.css';
-
+import { useRef } from 'react';
 function App() {
+  const homeRef = useRef(null)
+  const educationRef = useRef(null)
+  const workRef = useRef(null)
+  const portfolioRef = useRef(null)
   return (
     <>
-      <Nav />
+      <Nav homeRef={homeRef} educationRef={educationRef} workRef={workRef} portfolioRef={portfolioRef}/>
 
-      <div className='mb-40'>
-        <div className='w-2/4 mx-auto mt-10 mb-3'>
+      <div className='md:h-screen pt-10' ref={homeRef}>
+        <div className='md:w-2/4 w-10/12 mx-auto mt-10 mb-3'>
           <h1 className='text-5xl mb-3'>Fares Alharbi</h1>
           <p className='text-2xl text-cyan-900 ml-2'>Software Engineer</p>
         </div>
 
-        <div className='flex space-between w-2/4 m-auto py-5 border rounded-sm ppx-3 shadow'>
+        <div className='flex md:flex-row flex-col space-between md:w-2/4 m-auto py-5 border rounded-sm ppx-3 shadow'>
           <img alt='profile' className='rounded-full w-64 h-64 mx-8' src='./logo.png' />
           <div>
             <h5 className='text-2xl text-cyan-900 border-b-2 border-slate-300'>Biography</h5>
@@ -21,14 +25,14 @@ function App() {
         </div>
       </div>
 
-      <div className='mb-40'>
+      <div className='md:h-screen pt-16' ref={educationRef}>
         <div className='mb-5 mx-5'>
           <h1 className='text-5xl mb-3'>My Education</h1>
           <p className='text-lg text-cyan-900'>Here is the education that I have received</p>
         </div>
         
 
-        <div className='grid grid-cols-3 gap-4 mx5'>
+        <div className='grid md:grid-cols-3 grid-cols-1 gap-4 mx5'>
           <div className='border rounded-sm p-3 shadow'>
             <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>School, Computer Science</h3>
             <h5 className='py-2'>2017 - 2020</h5>
@@ -47,14 +51,14 @@ function App() {
         </div>
       </div>
 
-      <div className='mb-40'>
+      <div className='md:h-screen pt-16' ref={workRef}>
         <div className='mb-5 mx-5'>
           <h1 className='text-5xl mb-3'>My Work Experience</h1>
           <p className='text-lg text-cyan-900'>Here is some of my most recent relevant work experience</p>
         </div>
         
 
-        <div className='grid grid-cols-3 gap-4 mx5'>
+        <div className='grid md:grid-cols-3 gird-cols-1 gap-4 mx5'>
           <div className='border rounded-sm p-3 shadow'>
             <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Company, Software Engineer</h3>
             <h5 className='py-2'>2017 - 2020</h5>
@@ -73,14 +77,14 @@ function App() {
         </div>
       </div>
 
-      <div className='mb-40'>
+      <div className='md:h-screen pt-16' ref={portfolioRef}>
         <div className='mb-5 mx-5'>
           <h1 className='text-5xl mb-3'>My Portfolio</h1>
           <p className='text-lg text-cyan-900'>Here is some of my most recent projects that I have built and the  link code in Github</p>
         </div>
         
 
-        <div className='grid grid-cols-3 gap-4 mx5'>
+        <div className='grid md:grid-cols-3 grid-cols-1 gap-4 mx5'>
           <div className='border rounded-sm p-3 shadow'>
             <h3 className='text-lg border-b-2 border-slate-300 text-slate-800'>Tilte</h3>
             <h5 className='py-2 text-blue-500 hover:text-blue-700 transition cursor-pointer'>View Code</h5>
